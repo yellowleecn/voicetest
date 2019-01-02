4662f5522a545b0e4e5db804fda6d80939e25f5b
MQS123 <15207274447@163.com>
2019-01-02 16:57:43 +0800

    test

--- voicetest1/src/main/java/cn/linkedos/voicetest/VoicetestApplication.java
+++ voicetest1/src/main/java/cn/linkedos/voicetest/VoicetestApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VoicetestApplication {
-
+	//sssss
 	public static void main(String[] args) {
 		SpringApplication.run(VoicetestApplication.class, args);
 	}
